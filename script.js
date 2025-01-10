548c3eedf5e0b7929a9470723c8fdfaffabd15e7
dreuzzz <d@>
2025-01-10 22:55:17 +0100

    update

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
 function darkMode() {
     document.documentElement.classList.toggle("dark");
 }
-
-// Responsive Design
-
-var displayDimension = window.innerWidth;
-setInterval(checkWidth, 75);
-
-function checkWidth() {
-    document.getElementById("searchIcon").style.marginLeft = displayDimension - 80 + "px";
-    document.getElementById("switchTheme").style.marginLeft = displayDimension - 136 + "px";
-
-
-    if (window.innerWidth !== displayDimension) {
-        displayDimension = window.innerWidth;
-    }
-    console.log(displayDimension)
-}
